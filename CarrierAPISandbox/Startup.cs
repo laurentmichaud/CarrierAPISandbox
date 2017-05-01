509ac5c1fcb892d7625ac2d7e47087f944e07bf6
Laurent Michaud <Laurent Michaud>
2017-05-02 00:32:20 +0200

    Add project files.

---
+++ CarrierAPISandbox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarrierAPISandbox.Startup))]
+namespace CarrierAPISandbox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
